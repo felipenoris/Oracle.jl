6d6cc9fc3c8a91136b2f68bdbb677e7183017c5c
Felipe Noronha <felipenoris@gmail.com>
2018-12-31 17:30:09 -0300

    Build ODPI-C

--- src/Oracle.jl
+++ src/Oracle.jl
@@ -1,5 +1,15 @@
 module Oracle
 
+const DEPS_FILE = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(DEPS_FILE)
+    error("Oracle.jl is not installed properly, run Pkg.build(\"Oracle\") and restart Julia.")
+end
+include(DEPS_FILE)
+
+function __init__()
+    check_deps() # defined in DEPS_FILE
+end
+
 greet() = print("Hello World!")
 
 end # module
