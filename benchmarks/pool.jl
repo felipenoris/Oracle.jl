bff90f6c0a99d202d1fe54708d5c0d29e86826e1
Felipe Noronha <felipenoris@gmail.com>
2019-01-28 19:29:26 -0200

    improve pool bench

--- benchmarks/pool.jl
+++ benchmarks/pool.jl
@@ -43,9 +43,23 @@ end
 let
     ctx = Oracle.Context()
     println("Time to create a Pool")
-    @time pool = Oracle.Pool(ctx, username, password, connect_string, max_sessions=NUM_CONNECTIONS)
+    @time pool = Oracle.Pool(ctx, username, password, connect_string, max_sessions=NUM_CONNECTIONS, session_increment=1)
 
-    println("Time to acquire $NUM_CONNECTIONS connections from the pool")
+    println("Time to acquire $NUM_CONNECTIONS connections from the pool 1st time")
+
+    @time let
+        v = Vector{Oracle.Connection}()
+
+        for i in 1:NUM_CONNECTIONS
+            push!(v, Oracle.Connection(pool))
+        end
+
+        for c in v
+            Oracle.close!(c)
+        end
+    end
+
+    println("Time to acquire $NUM_CONNECTIONS connections from the pool 2nd time")
 
     @time let
         v = Vector{Oracle.Connection}()
