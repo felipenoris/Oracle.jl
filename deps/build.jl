2c7bdaad8832f4647c79f0eca23fdf78cb05e3b2
Felipe Noronha <felipenoris@gmail.com>
2019-03-09 12:04:35 -0300

    use a patched version of odpi with dpiNumber

--- deps/build.jl
+++ deps/build.jl
@@ -14,8 +14,14 @@ const DEPS_FILE = joinpath(@__DIR__, "deps.jl")
 const SRC_DIR = joinpath(PREFIX, "src")
 const LIB_DIR = joinpath(PREFIX, "lib")
 
-const ODPI_VERSION_NUMBER = "3.1.2"
-const ODPI_SOURCE_URL = "https://github.com/oracle/odpi/archive/v$(ODPI_VERSION_NUMBER).tar.gz"
+# upstream odpi
+#const ODPI_VERSION_NUMBER = "3.1.2"
+#const ODPI_SOURCE_URL = "https://github.com/oracle/odpi/archive/v$(ODPI_VERSION_NUMBER).tar.gz"
+
+# patched odpi
+const ODPI_VERSION_NUMBER = "3.1.2-dev-numeric"
+const ODPI_SOURCE_URL = "https://github.com/felipenoris/odpi/archive/v$(ODPI_VERSION_NUMBER).tar.gz"
+
 const ODPI_SOURCE_LOCAL_FILEPATH = joinpath(DOWNLOADS, "odpi_source.tar.gz")
 
 @static if _is_linux()
